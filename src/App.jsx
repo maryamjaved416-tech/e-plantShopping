c1eab106b609db969cc8fdfd806cb3605ef7b29a
maryamjaved416-tech <maryamjaved416@gmail.com>
2026-07-25 16:09:27 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Paradise Nursery</h1>
+      <p>Your one-stop shop for beautiful, healthy plants.</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
