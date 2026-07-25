767ec0f6610d0375f5c0c0598271db24927657e4
maryamjaved416-tech <maryamjaved416@gmail.com>
2026-07-25 15:56:34 +0500

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Us</h1>
+      <p>
+        Paradise Nursery is dedicated to bringing nature into your home. 
+        We offer a curated selection of healthy, high-quality plants 
+        to enhance your living space and improve your wellbeing.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
